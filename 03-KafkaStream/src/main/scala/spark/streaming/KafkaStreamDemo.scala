ff5ba037ee61be6acaa0a1dfbd6bb5ff836ff11c
Bhaskar Das <thebhaskardas@outlook.com>
2024-06-18 20:57:16 +0530

    Restructured

--- 03-KafkaStream/src/main/scala/spark/streaming/KafkaStreamDemo.scala
+++ 03-KafkaStream/src/main/scala/spark/streaming/KafkaStreamDemo.scala
@@ -18,34 +18,36 @@ object KafkaStreamDemo extends Serializable {
       .getOrCreate()
 
     val schema = StructType(List(
-      StructField("InvoiceNumber", StringType),
-      StructField("CreatedTime", LongType),
-      StructField("StoreID", StringType),
-      StructField("PosID", StringType),
-      StructField("CashierID", StringType),
-      StructField("CustomerType", StringType),
-      StructField("CustomerCardNo", StringType),
-      StructField("TotalAmount", DoubleType),
-      StructField("NumberOfItems", IntegerType),
-      StructField("PaymentMethod", StringType),
-      StructField("CGST", DoubleType),
-      StructField("SGST", DoubleType),
-      StructField("CESS", DoubleType),
-      StructField("DeliveryType", StringType),
-      StructField("DeliveryAddress", StructType(List(
-        StructField("AddressLine", StringType),
-        StructField("City", StringType),
-        StructField("State", StringType),
-        StructField("PinCode", StringType),
-        StructField("ContactNumber", StringType)
-      ))),
-      StructField("InvoiceLineItems", ArrayType(StructType(List(
-        StructField("ItemCode", StringType),
-        StructField("ItemDescription", StringType),
-        StructField("ItemPrice", DoubleType),
-        StructField("ItemQty", IntegerType),
-        StructField("TotalValue", DoubleType)
-      )))),
+                                StructField("InvoiceNumber", StringType),
+                                StructField("CreatedTime", LongType),
+                                StructField("StoreID", StringType),
+                                StructField("PosID", StringType),
+                                StructField("CashierID", StringType),
+                                StructField("CustomerType", StringType),
+                                StructField("CustomerCardNo", StringType),
+                                StructField("TotalAmount", DoubleType),
+                                StructField("NumberOfItems", IntegerType),
+                                StructField("PaymentMethod", StringType),
+                                StructField("CGST", DoubleType),
+                                StructField("SGST", DoubleType),
+                                StructField("CESS", DoubleType),
+                                StructField("DeliveryType", StringType),
+                                StructField("DeliveryAddress",
+                StructType(List(
+                                StructField("AddressLine", StringType),
+                                StructField("City", StringType),
+                                StructField("State", StringType),
+                                StructField("PinCode", StringType),
+                                StructField("ContactNumber", StringType)
+              ))),
+                StructField("InvoiceLineItems",
+                ArrayType(StructType(List(
+                                StructField("ItemCode", StringType),
+                                StructField("ItemDescription", StringType),
+                                StructField("ItemPrice", DoubleType),
+                                StructField("ItemQty", IntegerType),
+                                StructField("TotalValue", DoubleType)
+              )))),
     ))
 
     val kafkaDF = spark
